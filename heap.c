eb4c4242446442aadee02eeddcdb05bae74369db
Agussasd <56332666+Agussasd@users.noreply.github.com>
2019-11-30 18:48:34 -0300

    Update heap.c

--- heap.c
+++ heap.c
@@ -3,57 +3,53 @@
 #include <stdlib.h>
 #include <string.h>
 #include "heap.h"
-
+ 
 #define TAM_INICIAL 11
 #define REDIMENSION 2
 #define REDIMENSION_ABAJO 4
-
-//--------------------- FUNCION DE COMPARACION ---------------------//
-
-typedef int (*cmp_func_t) (const void *a, const void *b);
-
+ 
 //--------------------- STRUCTS ---------------------//
-
+ 
 struct heap{
 	void** datos;
 	int cantidad;
 	int capacidad;
 	cmp_func_t cmp;
 };
-
+ 
 //--------------------- FUNCIONES AUXILIARES ---------------------//
-
+ 
 void swap(void** arreglo, int a, int b){
 	void* aux = arreglo[a];
 	arreglo[a] = arreglo[b];
 	arreglo[b] = aux;
 }
-
+ 
 int pos_padre(int pos){
 	return (pos - 1) / 2;
 }
-
+ 
 int pos_hijo_izq(int pos){
 	return (pos * 2) + 1;
 }
-
+ 
 int pos_hijo_der(int pos){
 	return (pos * 2) + 2;
 }
-
+ 
 void upheap(void** arreglo, int pos, cmp_func_t cmp){
 	if(pos == 0){
 		return;
 	}
 	int padre = pos_padre(pos);
-	if(cmp(arreglo[padre], arreglo[pos]) > 0){
+	if(cmp(arreglo[pos], arreglo[padre]) > 0){
 		swap(arreglo, padre, pos);
 		upheap(arreglo, padre, cmp);
 	}
 }
-
+ 
 void downheap(void** arreglo, int capacidad, int pos, cmp_func_t cmp){
-	if(pos > capacidad){
+	if(pos >= capacidad){
 		return;
 	}
 	int max = pos; // Padre
@@ -70,9 +66,9 @@ void downheap(void** arreglo, int capacidad, int pos, cmp_func_t cmp){
 		downheap(arreglo, capacidad, max, cmp);
 	}
 }
-
-void** copiar_datos(void* arreglo[], size_t n){
-	void** copia_arreglo = malloc(n * sizeof(void*));
+ 
+void** copiar_datos(void* arreglo[], int n){
+	void** copia_arreglo = malloc((size_t)n * sizeof(void*));
 	if(!copia_arreglo){
 		return NULL;
 	}
@@ -81,56 +77,63 @@ void** copiar_datos(void* arreglo[], size_t n){
 	}
 	return copia_arreglo;
 }
-
+ 
 bool redimensionar(heap_t* heap, int tam){
 	void** copia = copiar_datos(heap->datos, heap->cantidad);
 	heap->datos = copia;
 	heap->capacidad = heap->capacidad * tam;
 	return true;
 }
-
+ 
 //--------------------- HEAP SORT ---------------------
-
+ 
 void heapify(void *arreglo[], int cantidad, cmp_func_t cmp){
-	for (int i = cantidad; i > 0; i--){
+	for(int i = cantidad; i > 0; i--){
 		downheap(arreglo, cantidad, i - 1, cmp);
 	}
 }
-
+ 
 void heap_sort(void *elementos[], size_t cant, cmp_func_t cmp){
-	
+	heapify(elementos, (int)cant, cmp);
+	int i = 0;
+	for(; i < cant; i++){
+		swap(elementos, 0, (int)cant - 1 - i);
+		downheap(elementos, (int)cant - 1 - i, 0, cmp);
+	}
 }
-
+ 
 //--------------------- PRIMITIVAS ---------------------//
-
+ 
 heap_t *heap_crear(cmp_func_t cmp){
 	heap_t* heap = malloc(sizeof(heap_t));
 	if(!heap){
 		return NULL;
 	}
+	void** datos = malloc(sizeof(void*) * TAM_INICIAL);
+	heap->datos = datos;
 	heap->cantidad = 0;
 	heap->capacidad = TAM_INICIAL;
 	heap->cmp = cmp;
 	return heap;
 }
-
+ 
 heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp){
 	heap_t* heap = heap_crear(cmp);
 	if(!heap){
 		return NULL;
 	}
-	void** copia_datos = copiar_datos(arreglo, n);
+	void** copia_datos = copiar_datos(arreglo, (int)n);
 	if(!copia_datos){
 		return NULL;
 	}
-	heapify(copia_datos, n, cmp);
+	heapify(copia_datos, (int)n, cmp);
 	heap->datos = copia_datos;
-	heap->cantidad = n;
-	heap->capacidad = n * 2;
+	heap->cantidad = (int)n;
+	heap->capacidad = (int)n * 2;
 	heap->cmp = cmp;
 	return heap;
 }
-
+ 
 void heap_destruir(heap_t *heap, void destruir_elemento(void *e)){
 	int cantidad = heap->cantidad, i = 0;
 	if(destruir_elemento){
@@ -141,11 +144,11 @@ void heap_destruir(heap_t *heap, void destruir_elemento(void *e)){
 	free(heap->datos);
 	free(heap);
 }
-
+ 
 size_t heap_cantidad(const heap_t *heap){
-	return heap->cantidad;
+	return (size_t)heap->cantidad;
 }
-
+ 
 bool heap_esta_vacio(const heap_t *heap){
 	if(heap->cantidad == 0){
 		return true;
@@ -154,7 +157,7 @@ bool heap_esta_vacio(const heap_t *heap){
 		return false;
 	}
 }
-
+ 
 bool heap_encolar(heap_t *heap, void *elem){
 	if(!elem){
 		return false;
@@ -176,16 +179,15 @@ bool heap_encolar(heap_t *heap, void *elem){
 	heap->cantidad++;
 	return true;
 }
-
+ 
 void *heap_ver_max(const heap_t *heap){
 	if(heap_esta_vacio(heap) == true){
 		return NULL;
 	}
-	else{
-		return heap->datos[0];
-	}
-}
+	return heap->datos[0];
 
+}
+ 
 void *heap_desencolar(heap_t *heap){
 	if(heap_esta_vacio(heap) == true){
 		return NULL;
@@ -194,12 +196,12 @@ void *heap_desencolar(heap_t *heap){
 	swap(datos, 0, heap->cantidad - 1);
 	void* dato = datos[heap->cantidad - 1];
 	datos[heap->cantidad - 1] = NULL;
-	downheap(datos, heap->cantidad, 0, heap->cmp);
 	heap->cantidad--;
+	downheap(datos, heap->cantidad, 0, heap->cmp);
 	if(heap->cantidad < (heap->capacidad) / REDIMENSION_ABAJO && heap->capacidad > TAM_INICIAL){
 		redimensionar(heap, heap->capacidad / REDIMENSION);
 	}
 	return dato;
 }
-
+ 
 //--------------------------------------------------------------//
